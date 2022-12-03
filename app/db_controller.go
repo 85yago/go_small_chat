4deee3fc56f1742d52b5d7f8c9582f929efacaa3
iorar <57796209+iorar@users.noreply.github.com>
2022-12-03 18:24:29 +0000

    コメント挿入

--- app/db_controller.go
+++ app/db_controller.go
@@ -11,6 +11,8 @@ const MSG_MAX = 30
 
 func write_db(db gorm.DB, message pkg_dbinit.Message) string {
 	db.Create(message)
+
+	// 最新MSG_MAX件から溢れたものを論理削除
 	db.Not(db.Order("CreatedAt desc limit " + strconv.Itoa(MSG_MAX))).Delete(&pkg_dbinit.Message{})
 
 	return "ここにエラーメッセージ"
