5658c13688d380778bf169fa2f69227d992e1f51
85yago <54394665+85yago@users.noreply.github.com>
2022-12-09 23:22:02 +0900

    修正とロックの追加

--- app/main.go
+++ app/main.go
@@ -4,10 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 )
+
+type ClientMessage struct {
+	Method  string
+	Name    string
+	Message string
+}
+
+type WsMap struct {
+	sync.RWMutex
+	m map[int]*websocket.Conn
+}
 
 var upgrader = websocket.Upgrader{
 	// TODO: ここ絶対直すこと
@@ -16,30 +28,81 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+func procClient(c *gin.Context, ws *websocket.Conn) {
+	for {
+		// メッセージを読む
+		mt, message, err := ws.ReadMessage()
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		// jsonのパース
+		var clientMsg ClientMessage
+		err = json.Unmarshal(message, &clientMsg)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+
+		// 送られたjson読んでクライアントがどっちを呼び出してるか判定
+		switch clientMsg.Method {
+		case "getMessage":
+			// TODO: getMessage関数に変える
+			message = []byte("get")
+		case "postMessage":
+			// TODO: postMessage関数に変える、ClientMessage型で渡す
+			message = []byte("post")
+		default:
+			// TODO: methodエラーを入れる
+			message = []byte(c.ClientIP())
+		}
+
+		// クライアントに返す
+		err = ws.WriteMessage(mt, message)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+}
+
+func broadcastMsg(wsMap *WsMap, c <-chan string) {
+	// このmapをgoroutineで回してbroadcast、これは更新があったら回すのを生やすって感じでよさそう？要検討
+
+	// チャネルにメッセージが放り込まれるの待ち
+	// interface定義してちゃんとそっちでやるとWriteJSONが使えると思う
+	mess := <-c
+
+	wsMap.RLock()
+	for _, ws := range wsMap.m {
+		err := ws.WriteMessage(websocket.TextMessage, []byte(mess))
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+	}
+	wsMap.RUnlock()
+
+	// closeをどうするか->とりあえず後回し、wsが生きてるか確認できるのでは？
+	// ping pongで確認できるじゃん、送る前に確認する or 別ルーチンで確認と削除
+}
+
 func main() {
-	// TODO: DBの初期化をする
+	// DBの初期化をする
+	// db := pkg_dbinit.DbInitialization()
+
+	// ginの初期化
 	r := gin.Default()
 
 	// broadcast用のmap生やしてGETの中でwsを保存しておく
-	var wsMap map[int]*websocket.Conn
+	var wsIndex int = 0
+	var wsMap = WsMap{m: make(map[int]*websocket.Conn)}
 
-	// TODO: broadcast用の関数を書いてチャネルを渡しておく、でそれに更新来たメッセージを投げる
-
-	// このmapをgoroutineで回してbroadcast、これは更新があったら回すのを生やすって感じでよさそう？要検討
-	go func(wsMap *map[int]*websocket.Conn, c chan string) {
-		// チャネルにメッセージが放り込まれるの待ち
-		// interface定義してちゃんとそっちでやるとWriteJSONが使えると思う
-		mess := <-c
-		for _, ws := range *wsMap {
-			err := ws.WriteMessage(websocket.TextMessage, []byte(mess))
-			if err != nil {
-				fmt.Println(err)
-				break
-			}
-		}
-	}(&wsMap) // 引数不足、チャネルを作って渡すこと
-	// closeをどうするか->とりあえず後回し、wsが生きてるか確認できるのでは？
-	// ping pongで確認できるじゃん、送る前に確認する or 別ルーチンで確認と削除
+	// ブロードキャスト用のチャネル
+	broadcastChan := make(chan string)
+	// ブロードキャスト用の関数
+	go broadcastMsg(&wsMap, broadcastChan)
 
 	r.GET("/", func(c *gin.Context) {
 		// websocketで接続
@@ -49,42 +112,13 @@ func main() {
 			return
 		}
 		defer ws.Close()
+		// ブロードキャスト用にソケットを保存
+		wsMap.Lock()
+		wsMap.m[wsIndex] = ws
+		wsMap.Unlock()
 
-		for {
-			// メッセージを読む
-			mt, message, err := ws.ReadMessage()
-			if err != nil {
-				fmt.Println(err)
-				break
-			}
-
-			// jsonのパース
-			var rawJson map[string]any
-			err = json.Unmarshal(message, &rawJson)
-			if err != nil {
-				fmt.Println(err)
-				break
-			}
-
-			// クライアントがどっちを呼び出してるか判定
-			switch rawJson["method"] {
-			case "getMessage":
-				// TODO: getMessage関数に変える
-				message = []byte("get")
-			case "postMessage":
-				// TODO: postMessage関数に変える
-				message = []byte("post")
-			default:
-				message = []byte(c.ClientIP())
-			}
-
-			// クライアントに返す
-			err = ws.WriteMessage(mt, message)
-			if err != nil {
-				fmt.Println(err)
-				break
-			}
-		}
+		// クライアントからのwebsocketを処理
+		procClient(c, ws)
 	})
 
 	r.Run(":8080") // 8080でリッスン
