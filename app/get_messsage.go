4847c4df6cb7909dc006605da402fa96d8955f60
85yago <54394665+85yago@users.noreply.github.com>
2022-12-10 18:44:08 +0900

    getMessage関数を書いた

--- app/get_messsage.go
+++ app/get_messsage.go
@@ -1,7 +1,29 @@
 package main
 
-import "gorm.io/gorm"
+import (
+	"app/pkg_dbinit"
+	"fmt"
 
+	"gorm.io/gorm"
+)
+
+// クライアントに返す用の構造体にメッセージを入れて返す
 func getMessage(db *gorm.DB) GetRetMessage {
-	return GetRetMessage{}
+	// DBを読み込む
+	var rawMessages []pkg_dbinit.Message
+	err := read_db(db, &rawMessages)
+	if err != nil {
+		fmt.Println(err)
+		// エラーならerrorだけ書いて返す
+		return GetRetMessage{Status: "error"}
+	}
+
+	// retにメッセージを入れる
+	var ret GetRetMessage
+	for _, mess := range rawMessages {
+		ret.Message = append(ret.Message, RetMessage{Name: mess.Name, Message: mess.Message, CreatedTime: mess.CreatedAt})
+	}
+	ret.Count = len(ret.Message)
+
+	return ret
 }
